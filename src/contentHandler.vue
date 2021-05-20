549f0b1ca0333bcfdd2b447dd12701edca6367d3
Fare Haven <farehaven@PresH-MacBook.local>
2021-05-20 13:48:58 +0100

    login page updated

--- src/contentHandler.vue
+++ src/contentHandler.vue
@@ -1,23 +1,17 @@
 <template>
 <div>
-  <webapp-header/>
   <webapp-changeUserPasswordPage/>
-  <webapp-footer/>
 </div>
 
 </template>
 
 <script>
-import changeUserPasswordPage from './components/wishlist.vue'
-import header from './components/header.vue'
-import footer from './components/footer.vue'
+import changeUserPasswordPage from './components/userLogin.vue'
 
 export default {
   name: 'App',
   components: {
     'webapp-changeUserPasswordPage':changeUserPasswordPage,
-    'webapp-header':header,
-    'webapp-footer':footer,
   }
 }
 </script>
