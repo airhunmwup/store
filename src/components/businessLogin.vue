41646087aaa710189c3aa5fe8477b4ebba6bfb42
Fare Haven <farehaven@PresH-MacBook.local>
2021-05-21 13:36:54 +0100

    init

---
+++ src/components/businessLogin.vue
@@ -0,0 +1,88 @@
+<template>
+    <div id="page-404" class="blog">
+    <!-- main content -->
+    
+    <!-- main content -->
+    <div class="main-content">
+        <div id="wrapper-site">
+            <div id="content-wrapper">
+                <section class="page-home">
+                    <div class="container">
+                        <div class="row center">
+                            <div class="content-404 col-lg-6 col-sm-6 text-center">
+                                <a href="#">
+                                    <img class="img-fluid" src="img/home/logo.png" width="50%" alt="image 404 right">
+                                </a>
+                            </div>
+                            <div class="content-right-404 col-lg-5 col-sm-6 text-center">
+                                <h1 class="text-center title-page">Log In</h1>
+                        <div class="login-form">
+                            <form id="customer-form" action="#" method="post">
+                                <div class="container col-6">
+                                    <input type="hidden" name="back" value="my-account">
+                                    <div class="form-group no-gutters">
+                                        <input class="form-control" name="email" type="email" placeholder="Email">
+                                    </div>
+                                    <div class="form-group no-gutters">
+                                        <div class="input-group js-parent-focus">
+                                            <input class="form-control js-child-focus js-visible-password" name="password" type="password" value="" placeholder="Password">
+                                        </div>
+                                    </div>
+                                    <div class="no-gutters text-left">
+                                        <div class="forgot-password ">
+                                            <a href="user-reset-password.html" rel="nofollow" class="text-info">
+                                                Forgot your password?
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="clearfix">
+                                    <br>
+                                    <div class="text-left no-gutters">
+                                        <input type="hidden" name="submitLogin" value="1">
+                                        <button class="btn btn-default" data-link-action="sign-in" type="submit">
+                                            Submit
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="text-left">
+                            <br>
+                            <p>Don't have an account? <a href="#">Sign Up now</a></p>
+                        </div>
+                            </div>
+                        </div>
+                     </div>
+                </section>
+            </div>
+        </div>
+    </div>
+
+         <!-- breadcrumb -->
+         <nav class="breadcrumb-bg">
+           <div class="container no-index">
+                <div class="breadcrumb">
+                    <ol>
+                      <li>
+                         <a href="#">
+                             <span>All rights reserved <b>REJEE COMPANY LIMITED &trade;</b></span>
+                         </a>
+                     </li>
+                    </ol>
+                </div>
+            </div>
+        </nav>
+    </div>
+
+</template>
+<script>
+export default {
+  data() {
+      return{
+    footerText: 'Copyright REJEE.co.uk 2021'
+
+      }
+  }
+}
+</script>
--- src/contentHandler.vue
+++ src/contentHandler.vue
@@ -6,7 +6,7 @@
 </template>
 
 <script>
-import changeUserPasswordPage from './components/registerBusinessAccount.vue'
+import changeUserPasswordPage from './components/businessLogin.vue'
 
 export default {
   name: 'App',
