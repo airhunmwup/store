5068630f613dac7f322b00129b77076de6d52359
Precious Airh <68897397+airhunmwup@users.noreply.github.com>
2021-05-28 13:59:52 +0100

    collect repo

--- src/pages/sellers/seller_dashboard.vue
+++ src/pages/sellers/seller_dashboard.vue
@@ -15,62 +15,97 @@
 
             <div class="acount head-acount  container">
                 <div class="container">
-                    <div id="main box">
-                        <h1 class="title-page"> MY ORDER</h1>
-                        <p class="box">3 orders</p>
+                    <div id="main">
+                        <h1 class="title-page">Order Details</h1>
                             <table class="std table" id="block-history">
                                 <tbody>
                                     <tr>
                                         <th class="first_item">
                                             <p class="text-dark">IT'S DELIVERD!</p><span class="text-small">Delivered on 14 MAR 2021</span>
-                                            <div class="row content col-12">
-                                                    <img src="img/product/6.jpg" class="col-3 " alt="Product">
-                                                    <img src="img/product/9.jpg" class="col-3 " alt="Product">
-                                                    <img src="img/product/3.jpg" class="col-3 " alt="Product">
-                                                    <img src="img/product/4.jpg" class="col-3 " alt="Product">
-                                            </div>
-
-                                            <p>Order No: <span class="text-small"> 567328996</span></p>
-                                            <p>Shipping date: <span class="text-small"> 11 MAR 2021</span></p>
+                                            <hr>
+                                            <p><small>Your order has been delivered. We hope you love it!</small></p>
+                                            <br>
+                                            <p><small><b class="fa fa-folder"></b> Order No:  <span class="text-small"> 567328996</span></small></p>
+                                            <p><small><b class="fa fa-calender"></b> Order No:  <span class="text-small"> 9 MAR 2021</span></small></p>
+                                            <p><small><b class="fa fa-folder"></b> Order No:  <span class="text-small"> 11 MAR 2021</span></small></p>
                                         </th>
-                                        <td class="btn item"><b><i class="fa fa-angle-right"></i></b></td>
                                     </tr>
                                     <tr>
-                                        <th class="btn form-control text-success first_item">Track Order</th>
+                                        <th class="first_item btn text-success">Track Order</th>
+                                    </tr>
+                                    <tr>
+                                        <th class="first_item box"><b class="fa fa-info-o"></b><small>MORE INFO</small></th>
+                                    </tr>
+                                    <tr>
+                                        <th class="first_item box">DELIVERY DETAILS</th>
+                                    </tr>
+                                    <tr>
+                                        <th class="first_item">
+                                            <p>DELIVERY ADDRESS</p>
+                                            <p><small>David Johnson</small></p>
+                                            <p><small>139 Jervoise Road</small></p>
+                                            <p><small>West Midlands</small></p>
+                                            <p><small>Birmingham</small></p>
+                                            <p><small>UK</small></p>
+                                            <p><small>07746358183</small></p>
+                                        </th>
+                                    </tr>
+                                    <tr>
+                                        <th class="first_item">ESTIMATED DELIVERY
+                                            <p><small>Friday, 11 MAY 2021</small></p>
+                                        </th>
+                                    </tr>
+                                    <tr>
+                                        <th class="first_item">DELIVERY METHOD
+                                            <p><small>Fast Delivery</small></p>
+                                        </th>
                                     </tr>
                                 </tbody>
-
-                                
                             </table>
-                            <p class="box"><br></p>
-                            <table class="std table" id="block-history">
+
+                        <p class="box col"><b>Items (3)</b></p>
+                            <table class="std table" >
                                 <tbody>
                                     <tr>
                                         <th class="first_item">
-                                            <p class="text-dark">IT'S DELIVERD!</p><span class="text-small">Delivered on 14 MAR 2021</span>
                                             <div class="row content col-12">
-                                                    <img src="img/product/6.jpg" class="col-3 " alt="Product">
-                                                    <img src="img/product/9.jpg" class="col-3 " alt="Product">
-                                                    <img src="img/product/3.jpg" class="col-3 " alt="Product">
-                                                    <img src="img/product/4.jpg" class="col-3 " alt="Product">
-                                                    <img src="img/product/7.jpg" class="col-3 " alt="Product">
-                                                    <img src="img/product/10.jpg" class="col-3 " alt="Product">
-                                                    <img src="img/product/1.jpg" class="col-3 " alt="Product">
+                                                    <span class="col-3">
+                                                        <img src="img/product/6.jpg" class="" alt="Product">
+                                                        <p><span class="text-small"> 567328996</span></p>
+                                                        <p><span class="text-small"> 11 MAR 2021</span></p>
+                                                    </span>
+                                                    <span class="col-3">
+                                                        <img src="img/product/6.jpg" class="" alt="Product">
+                                                        <p><span class="text-small"> 567328996</span></p>
+                                                        <p><span class="text-small"> 11 MAR 2021</span></p>
+                                                    </span>
                                             </div>
-
-                                            <p>Order No: <span class="text-small"> 567328996</span></p>
-                                            <p>Shipping date: <span class="text-small"> 11 MAR 2021</span></p>
                                         </th>
-                                        <td class="btn item"><b><i class="fa fa-angle-right"></i></b></td>
+                                    </tr>
+
+                                    <tr>
+                                        <th class="first_item box">PAYMENT DETAIL</th>
                                     </tr>
                                     <tr>
-                                        <th class="btn text-success form-control first_item">Track Order</th>
+                                        <th class="first_item">
+                                            <p class="col text"><i class="fa fa-cc-visa"> VISA Debit</i></p>
+                                        </th>
+                                    </tr>
+                                    <tr>
+                                        <th class="first_item box">ORDER TOTAL</th>
+                                    </tr>
+                                    <tr>
+                                        <th class="first_item">
+
+                                            <p>Sub-total:  <span class="text-small"> £12.60</span></p>
+                                            <p>Delivery:  <span class="text-small"> FREE</span></p>
+                                            <p><b>TOTAL: <span class="text-small"> £12.60</span></b></p>
+                                        </th>
                                     </tr>
                                 </tbody>
 
-                                
                             </table>
-                            <p class="box"><br></p>
+
                     </div>
 
                 </div>
