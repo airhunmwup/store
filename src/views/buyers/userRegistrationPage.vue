33669e0bb75417425fe58e372b4ee1ebbe7c139c
Precious Airh <68897397+airhunmwup@users.noreply.github.com>
2021-07-18 13:18:59 +0100

    loader added as required on reg page

--- src/views/buyers/userRegistrationPage.vue
+++ src/views/buyers/userRegistrationPage.vue
@@ -199,6 +199,16 @@
     </div>
 
     <hr />
+
+    <div id="page-preloader">
+        <div class="page-loading">
+            <div class="dot"></div>
+            <div class="dot"></div>
+            <div class="dot"></div>
+            <div class="dot"></div>
+            <div class="dot"></div>
+        </div>
+    </div>
   </div>
 </template>
 <script>
