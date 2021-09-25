742cbbe89e7bc520cb8f562b0c4bb9153f7c31c4
Precious Airh <68897397+airhunmwup@users.noreply.github.com>
2021-09-25 11:17:55 +0100

    App Update

--- src/views/sellers/registerBusinessAccount.vue
+++ src/views/sellers/registerBusinessAccount.vue
@@ -26,18 +26,18 @@
                 <div class="row col-sm-12 col-lg-4 col-md-4"><form>
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Business name</label>
-    <input type="text" class="form-control">
-    <div class="form-text">error code here</div>
+    <input name="First_Name" type="text" value="" placeholder="First Name" v-model="formData.first_name" class="form-control">
+    <div class="form-text"><p class="text-danger text-left" v-text="errors.first_name"></p></div>
   </div>
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <div class="form-text">error code here</div>
+    <input  name="Email" type="email" value="" placeholder="Email" v-model="formData.email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div class="form-text"><p class="text-danger text-left" v-text="errors.email"></p></div>
   </div>
   <div class="mb-3">
     <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
-    <div class="form-text">error code here</div>
+    <input type="password" class="form-control" id="exampleInputPassword1" name="Pass_word" value="" placeholder="Password" v-model="formData.password">
+    <div class="form-text"><p class="text-danger text-left" v-text="errors.password"></p></div>
   </div>
   <div class="mb-3 form-check">
     <input type="checkbox" class="form-check-input" id="exampleCheck1">
@@ -57,7 +57,7 @@
                                 title="User Privacy"
                               >User Privacy Notice</router-link></label>
   </div><div class="d-grid gap-2">
-  <button class="btn btn-warning bg-gradient border" type="button">Create account</button>
+  <button class="btn btn-warning bg-gradient border" type="button" @click='userRegister'>Create account</button>
 </div>
 </form>
       </div>
@@ -78,3 +78,42 @@
 
 
 </template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      formData: {
+        first_name: "",
+        last_name: "Store",
+        email: "",
+        password: "",
+        user_type: "seller"
+      },
+      errors: {},
+      loading: false
+    };
+  },
+  methods: {
+    userRegister() {
+      this.loading = true;
+      this.$http
+      .post('http://localhost:8000/api/register', this.formData).then((response) =>{
+          console.log(response.data);
+          this.formData.first_name = this.formData.last_name = this.formData.email = this.formData.password = ''
+          this.errors = {}
+          this.$router.push('/login');
+      }).catch((errors) => {
+          this.errors = {};
+          const err = Object.keys(errors.response.data.errors);
+          err.forEach((key)=>{
+            var strError = errors.response.data.errors[key][0];
+            this.errors[key] = strError;
+          })
+          
+      }).finally(() => this.loading = false);
+    },
+  }
+};
+</script>
