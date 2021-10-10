4eca41dd150e1ce3284abe2d9e109dfe0592389e
Precious Airh <68897397+airhunmwup@users.noreply.github.com>
2021-10-10 17:32:43 +0100

    init update

--- src/views/buyers/vehicles.vue
+++ src/views/buyers/vehicles.vue
@@ -123,13 +123,133 @@
     <input type="number" class="form-control m-1 text-xs" placeholder="max">
   </div>
     </div>
+    <div class="col-12 col-md-4 row">
+    <label for="inputminprice" class="m-1 font-weight-bold text-xs">Year</label>
+  <div class="col-5">
+    <select class="form-control m-1 text-xs" >
+                  <option value="">min</option>
+                  <option value="2022">2022</option>
+                  <option value="2021">2021</option>
+                  <option value="2020">2020</option>
+                  <option value="2019">2019</option>
+                  <option value="2018">2018</option>
+                  <option value="2017">2017</option>
+                  <option value="2016">2016</option>
+                  <option value="2015">2015</option>
+                  <option value="2014">2014</option>
+                  <option value="2013">2013</option>
+                  <option value="2012">2012</option>
+                  <option value="2011">2011</option>
+                  <option value="2010">2010</option>
+                  <option value="2009">2009</option>
+                  <option value="2008">2008</option>
+                  <option value="2007">2007</option>
+                  <option value="2006">2006</option>
+                  <option value="2005">2005</option>
+                  <option value="2004">2004</option>
+                  <option value="2003">2003</option>
+                  <option value="2002">2002</option>
+                  <option value="2001">2001</option>
+                  <option value="2000">2000</option>
+                  <option value="1999">1999</option>
+                  <option value="1998">1998</option>
+                  <option value="1997">1997</option>
+                  <option value="1996">1996</option>
+                  <option value="1995">1995</option>
+                  <option value="1994">1994</option>
+                  <option value="1993">1993</option>
+                  <option value="1992">1992</option>
+                  <option value="1991">1991</option>
+                  <option value="1990">1990</option>
+                  <option value="1989">1989</option>
+                  <option value="1988">1988</option>
+                  <option value="1987">1987</option>
+                  <option value="1986">1986</option>
+                  <option value="1985">1985</option>
+                  <option value="1984">1984</option>
+                  <option value="1983">1983</option>
+                  <option value="1982">1982</option>
+                  <option value="1981">1981</option>
+                  <option value="1980">1980</option>
+                  <option value="1979">1979</option>
+                  <option value="1978">1978</option>
+                  <option value="1977">1977</option>
+                  <option value="1976">1976</option>
+                  <option value="1975">1975</option>
+                  <option value="1976">1976</option>
+                </select>
+  </div>
+  <div class="col-5">
+    <select class="form-control m-1 text-xs" >
+                  <option value="">max</option>
+                  <option value="2022">2022</option>
+                  <option value="2021">2021</option>
+                  <option value="2020">2020</option>
+                  <option value="2019">2019</option>
+                  <option value="2018">2018</option>
+                  <option value="2017">2017</option>
+                  <option value="2016">2016</option>
+                  <option value="2015">2015</option>
+                  <option value="2014">2014</option>
+                  <option value="2013">2013</option>
+                  <option value="2012">2012</option>
+                  <option value="2011">2011</option>
+                  <option value="2010">2010</option>
+                  <option value="2009">2009</option>
+                  <option value="2008">2008</option>
+                  <option value="2007">2007</option>
+                  <option value="2006">2006</option>
+                  <option value="2005">2005</option>
+                  <option value="2004">2004</option>
+                  <option value="2003">2003</option>
+                  <option value="2002">2002</option>
+                  <option value="2001">2001</option>
+                  <option value="2000">2000</option>
+                  <option value="1999">1999</option>
+                  <option value="1998">1998</option>
+                  <option value="1997">1997</option>
+                  <option value="1996">1996</option>
+                  <option value="1995">1995</option>
+                  <option value="1994">1994</option>
+                  <option value="1993">1993</option>
+                  <option value="1992">1992</option>
+                  <option value="1991">1991</option>
+                  <option value="1990">1990</option>
+                  <option value="1989">1989</option>
+                  <option value="1988">1988</option>
+                  <option value="1987">1987</option>
+                  <option value="1986">1986</option>
+                  <option value="1985">1985</option>
+                  <option value="1984">1984</option>
+                  <option value="1983">1983</option>
+                  <option value="1982">1982</option>
+                  <option value="1981">1981</option>
+                  <option value="1980">1980</option>
+                  <option value="1979">1979</option>
+                  <option value="1978">1978</option>
+                  <option value="1977">1977</option>
+                  <option value="1976">1976</option>
+                  <option value="1975">1975</option>
+                  <option value="1976">1976</option>
+                </select>
+  </div>
     <div class="col-12 col-md-4">
     <label for="inputcondition" class="m-1 font-weight-bold text-xs">Items condition</label>
-    <select type="number" id="inputcondition" class="form-control m-1 col-5 text-xs" placeholder="max">
+    <select type="number" id="inputcondition" class="form-control m-1 col-5 text-xs" placeholder="">
       <option>Any</option>
       <option>New</option>
       <option>Used</option>
     </select>
+    </div>
+    <div class="col-12 col-md-4">
+    <label for="inputcondition" class="m-1 font-weight-bold text-xs">Engine type</label>
+    <select type="number" id="inputcondition" class="form-control m-1 col-5 text-xs" placeholder="">
+                  <option value=""></option>
+                  <option value="Manual transmission">Manual transmission</option>
+                  <option value="Automatic transmission">Automatic transmission</option>
+                  <option value="Hybrid">Hybrid</option>
+    </select>
+    </div>
     </div>
     <div class="col-12 col-md-4 m-2 pt-4 center">
 <button type="button" class="btn btn-warning text-sm btn-sm border btn-block">See Listings</button>
