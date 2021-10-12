e3545ce99d050632516cebc9e501dd2fac43433c
Precious Airh <68897397+airhunmwup@users.noreply.github.com>
2021-10-12 15:09:08 +0100

    init update

--- src/views/buyers/checkOutPageAddress.vue
+++ src/views/buyers/checkOutPageAddress.vue
@@ -7,6 +7,53 @@
     <!-- main -->
 
     <div class="row p-4 text-sm">
+        <div class="col-4 col-xs-12">
+            <p class="text-dark h4">Order Items</p>
+            {{ this.$store.state.basket.length }} Item(s)
+            <div
+          class="text-sm"
+          v-for="cart in this.$store.state.basket"
+          :key="cart.id"
+          >
+                <p> {{ cart["product_name"] }} x {{ cart["qnty"] }} - GBP £{{ cart["qnty_price"] }}</p>
+            </div>
+            <hr>
+            <b>Subtotal: £{{ this.$store.state.basketTotal }}</b> <br>
+            <b>Shipping: £{{this.$store.state.basket.product_shipping_cost }}</b> <hr>
+            <b>Total: £{{ parseInt(this.$store.state.basketTotal + this.$store.state.basket.product_shipping_cost)}}</b>
+        </div>
+        <div class="col-8">
+      <div class="col-12">
+        <p class="text-dark h4">Billing Details</p>
+      </div>
+            <div class="row">
+      <div class="col-6 col-xs-12">           
+           <label>First Name</label>
+           <input type="text" class="form-control"  v-model="formData.order_firstname" disabled />        
+      </div>
+      <div class="col-6 col-xs-12">           
+           <label>Last Name</label>
+           <input type="text" class="form-control"  v-model="formData.order_lastname" disabled />        
+      </div> 
+            </div>
+            <div class="row">
+                <div class="col-6 col-xs-12">           
+                    <label>Email</label>
+                    <input type="text" class="form-control"  v-model="formData.order_email" disabled/>        
+                </div>
+      <div class="col-6 col-xs-12">           
+           <label>Phone</label>
+           <input type="text" class="form-control"  v-model="formData.order_phone"/>        
+      </div>  
+      
+            </div>
+            <div class="row">
+      <div class="col-12">           
+           <label>Order Notes</label>
+           <textarea type="text" class="form-control" v-model="formData.order_notes"></textarea>        
+      </div>
+            </div>
+      </div>      
       <div class="col-12">
         <p class="text-dark h4">Select a delivery address</p>
       </div>
@@ -24,7 +71,7 @@
       <div class="dropdown-divider"></div>
 
       <div class="row">
-        <div class="m-2 col-sm-4">
+        <div class="m-2 col-sm-12">
           <div class="card">
             <div class="card-body">
               <h5 class="card-title text-dark h6">{{this.$store.state.full_name}}</h5>
@@ -36,18 +83,6 @@
               <p class="card-text text-dark">{{this.$store.state.address[0].town_city}}, {{this.$store.state.address[0].postal_code}}</p>
 
               <p class="card-text text-dark">{{this.$store.state.address[0].country}}</p>
-            </div>
-
-            <p class="">
-              <router-link
-                  class="text-sm m-1 col-7 btn border btn-sm btn-warning"
-                to="/checkoutpayment"
-                data-toggle="collapse"
-                data-target=".navbar-collapse"
-                title="Home"
-                >
-                  Deliver to this address
-              </router-link>
               <button
                 type="button"
                 class="
@@ -61,6 +96,20 @@
               >
                 Edit
               </button>
+            </div>
+            
+            <p class="text-center">
+              <router-link
+                  class="text-sm m-1 col-12 btn border btn-sm btn-warning"
+                style="width: 90%;"  
+                to="/checkoutpayment"
+                data-toggle="collapse"
+                data-target=".navbar-collapse"
+                title="Home"
+                >
+                  create order
+              </router-link>
+              
             </p>
           </div>
         </div>
@@ -70,3 +119,51 @@
     </div>
   </div>
 </template>
+<script>
+import User from '../../apis/User';
+export default {
+  data() {
+    return {
+      formData: {       
+        order_userid: this.$store.state.currentUser.id,
+        order_firstname : this.$store.state.currentUser.first_name,
+        order_lastname : this.$store.state.currentUser.last_name,
+        order_phone: "",
+        order_email: this.$store.state.currentUser.email,
+        order_notes: "",
+      },
+      
+      errors: {},
+      loading: false
+    };
+  },
+  methods: {
+    uploadImage(event) {
+      const file = event.target.files[0];
+      if(file){
+        if(this.num <= 4){
+          this.image.push(file);
+          this.formData.imageData.push(file);
+          this.formData.testImage = file;
+          this.imagePreview.push(URL.createObjectURL(file));
+          this.num++;
+        }else{
+          console.log('max 4');
+        }
+      }
+    },
+
+    errorStyle(err){
+      if(err){
+        return {
+          color: 'red',
+          fontSize: '12px'
+        }
+      }
+    }
+  },
+  computed: {
+
+  }
+}
+</script>
